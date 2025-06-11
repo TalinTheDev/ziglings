aa2dedb3664dbf4fd6b078a50ec8ee2a7bd5c032
Talin Sharma <talinsharma.dev@gmail.com>
2025-06-11 16:30:48 -0400

    More container stuff including nicer containerSetup -> zigSetup script

--- .devcontainer/containerSetup.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-# Install ZIG & ZLS (master) using ZVM
-curl https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
-zvm i --zls --full master
-zvm use master
